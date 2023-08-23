3c59495c4c03a5619677850edd859119a6b14f61
poornima.v <poornima.v@pacewisdom.com>
2023-08-23 14:21:45 +0530

    Initial commit

---
+++ details_table.py
@@ -0,0 +1,78 @@
+import os
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import pandas as pd
+from selenium.webdriver.common.by import By
+from order_judgement import scrape_order_judgment_table
+import logging_utils
+import threading
+
+file_lock = threading.Lock()
+
+def scrape_data_from_details_page(driver, details_page_url, diary_number):
+    logger = logging_utils.setup_logging('scraping_log.log')
+    try:
+        driver.get(details_page_url)
+    
+
+        # Parse the page source with BeautifulSoup
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        
+        # Find the details table
+        table = soup.find("table", class_="table-bordered")
+
+        details_data = {}
+
+        # Loop through each row and extract the data
+        for row in table.find_all("tr"):
+            cells = row.find_all("td")
+            label = None
+            for i, cell in enumerate(cells):
+                cell_text = cell.get_text(strip=True)
+                if i % 2 == 0:
+                    # Cell contains a label
+                    label = cell_text
+                else:
+                    # Cell contains a value
+                    details_data[label] = cell_text
+            
+        filing_number_str = details_data.get("Filing Number", "0")
+        try:
+            filing_number = int(filing_number_str)
+        except ValueError:
+            logger.error("%s - Invalid Filing Number: %s", diary_number, filing_number_str)
+            filing_number = 0
+
+        df = pd.DataFrame([details_data]) 
+        csv_filename = "outputs/details.csv"
+        serial_number = 1
+
+        if os.path.isfile(csv_filename) and os.path.getsize(csv_filename) > 0:
+            existing_df = pd.read_csv(csv_filename)
+            last_serial_number = existing_df['S. No'].max()
+            if not pd.isna(last_serial_number):
+                serial_number = int(last_serial_number) + 1
+
+        if not df.empty:       
+            df.insert(0, "S. No", range(serial_number, serial_number + len(df)))
+            
+            with file_lock:
+                if not os.path.isfile(csv_filename) or os.path.getsize(csv_filename) == 0:
+                    df.to_csv(csv_filename, index=False, mode='w', encoding='utf-8')  # Save with header
+                else:
+                    df.to_csv(csv_filename, index=False, mode='a', header=False, encoding='utf-8')  # Append without header
+                    
+            logger.info("%s - Data from details page saved to %s", diary_number, csv_filename)
+        
+        else:
+            logger.info('%s - No Data Found on details page. Skipping current table', diary_number)
+        # Call another function if needed
+        scrape_order_judgment_table(driver, filing_number, diary_number)
+        
+    except NoSuchElementException as e:
+       logger.error("%s - Error while scraping details page: %s", diary_number, str(e))
+
+    except Exception as e:
+        logger.error("%s - Error while scraping details page: %s", diary_number, str(e))
---
+++ main.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from main_table import scrape_data_from_current_page
+from config_utils import config,  write_config
+import time
+import logging_utils
+import threading
+
+
+def process_range(start_diary_number, end_diary_number, logger, t):
+    try:
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_argument("--headless=new")
+        driver = webdriver.Chrome(options=options)
+
+
+        count = 1
+        for diary_number in range(start_diary_number, end_diary_number, 2):
+            config_copy = config.copy()
+            config_copy['diaryno'] = str(diary_number)
+            write_config(config_copy)
+            
+            logger.info("%s - Iteration - %d started for thread %d", config_copy["diaryno"], count, t)
+
+            start_time = time.time()
+            scrape_data_from_current_page(driver, config_copy)
+            end_time = time.time()
+
+            loop_time = end_time - start_time
+
+            logger.info("%s - Iteration - %d completed in %s seconds for thread %d", config_copy["diaryno"], count, round(loop_time, 2), t)
+            # time.sleep(5)
+            count += 1
+
+    except Exception as e:
+        logger.error("An error occurred %s", str(e))
+
+    finally:
+        driver.quit()
+
+if __name__ == "__main__":
+    logger = logging_utils.setup_logging('scraping_log.log')
+    total_time = 0
+    try:
+        
+        start_diary_number = int(config["diaryno"]) + 1  # Convert to integer
+        end_diary_number = start_diary_number + 4
+        start_time = time.time()
+        
+        t1 = 1
+        thread1 = threading.Thread(target = process_range, args = (start_diary_number, end_diary_number, logger, t1))
+        
+        t2 = 2
+        thread2 = threading.Thread(target = process_range, args = (start_diary_number + 1, end_diary_number, logger, t2))
+        
+        thread1.start()
+        thread2.start()
+
+        thread1.join()
+        thread2.join()
+
+        end_time = time.time()
+        total_time = end_time - start_time
+
+        logger.info("Total time taken for all iterations: %s seconds", round(total_time, 2))
+           
+    except WebDriverException as e:
+        if "ERR_INTERNET_DISCONNECTED" in str(e):
+            logger.error("Internet connection lost. Please check your connection.")
+        else:
+            logger.error("An error occurred: ", str(e))
+
+    except KeyboardInterrupt:
+        logger.error("Script interrupted by the user.")   
+
+    except Exception as e:
+        logger.error("An error occurred: %s", str(e))
+
